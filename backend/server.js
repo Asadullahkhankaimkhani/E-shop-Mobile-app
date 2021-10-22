9ba3f90690819c1aadb39632e5ebb0690559c5bf
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-10-22 05:17:27 +0500

    Setting Up Environmental  variable

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
+require("dotenv/config");
+
+const api = process.env.API_URL;
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get(`${api}/product`, (req, res) => {
   res.send("Hello Api");
 });
 
 app.listen(3000, (req, res) => {
+  console.log(`${api}/product`);
   console.log("Server is Running on Port 3000");
 });
