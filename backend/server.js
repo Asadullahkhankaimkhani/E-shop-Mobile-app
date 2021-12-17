60941e39c4d6e9b47e7d901039802a5bca135d2d
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2021-12-18 01:51:11 +0500

    Connected MongoDb Cloud

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,24 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
 require("dotenv/config");
 
 const api = process.env.API_URL;
 
+const dbConnect = async () => {
+  await mongoose.connect(process.env.CONNECTION_STRING);
+  console.log("### Database Connected Successfully ###");
+};
+
+dbConnect();
+
 const app = express();
 
-app.get(`${api}/product`, (req, res) => {
-  res.send("Hello Api");
-});
+// Middleware
+app.use(express.json());
+app.use(morgan("tiny"));
 
 app.listen(3000, (req, res) => {
-  console.log(`${api}/product`);
   console.log("Server is Running on Port 3000");
 });
