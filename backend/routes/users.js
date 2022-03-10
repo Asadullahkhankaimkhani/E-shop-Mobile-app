4da68b0f3d50a2840ff25879e503ebae7671d22b
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-03-10 11:58:53 +0500

    User and Authentication

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,31 @@
 const express = require("express");
 const router = express.Router();
-const { userList } = require("../controller/users");
+
+const {
+  userRegister,
+  userCount,
+  userCreate,
+  userDelete,
+  userGet,
+  userList,
+  userLogin,
+  userUpdate,
+} = require("../controller/users");
 
 router.get(`/users`, userList);
+
+router.get("/user/:id", userGet);
+
+router.post("/user", userCreate);
+
+router.put("/user/:id", userUpdate);
+
+router.post("/login", userLogin);
+
+router.post("/register", userRegister);
+
+router.delete("/user/:id", userDelete);
+
+router.get(`/get/count`, userCount);
 
 module.exports = router;
